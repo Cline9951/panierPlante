6a892c6216368c7a3ea4b5c2aff95ab68dbeda98
Cline9951 <cagan.cline@gmail.com>
2022-03-10 12:06:25 +0100

    course complete and ready to be continue

---
+++ src/components/Description.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Description() {
+    const text = "Ici achetez toutes les plantes dont vous avez toujours rêvé"
+    return (<p>{ text.toUpperCase() }</p>)
+}
+export default Description;
